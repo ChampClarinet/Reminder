bbd4bb8732d31825f95e57845563363b5595c44e
Wallop Opasakhun <champbassist159@gmail.com>
2016-12-07 22:41:40 +0700

    add CardView Content

--- app/src/main/java/com/example/clarinetmaster/reminder/Tools/EventList.java
+++ app/src/main/java/com/example/clarinetmaster/reminder/Tools/EventList.java
@@ -1,14 +1,13 @@
-package com.example.clarinetmaster.reminder.Models;
+package com.example.clarinetmaster.reminder.Tools;
 
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
 
 import com.example.clarinetmaster.reminder.Databases.DatabaseHelper;
-import com.example.clarinetmaster.reminder.Tools.DateTime;
+import com.example.clarinetmaster.reminder.Models.Event;
+import com.example.clarinetmaster.reminder.R;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -40,12 +39,13 @@ public class EventList {
     }
 
     public static void loadFromDatabase(){
-        mEventList.clear();
+        if(mEventList == null) mEventList = new ArrayList<>();
+        else mEventList.clear();
 
         mHelper = new DatabaseHelper(mContext);
         mDb = mHelper.getWritableDatabase();
 
-        Cursor cursor = mDb.query(mHelper.TABLE_NAME, null, null, null, null, null, "ORDER BY " + mHelper.COL_DATE);
+        Cursor cursor = mDb.query(mHelper.TABLE_NAME, null, null, null, null, null, mHelper.COL_DATE);
 
         while( cursor.moveToNext() ){
             int id = cursor.getInt(cursor.getColumnIndex(mHelper.COL_ID));
@@ -54,11 +54,33 @@ public class EventList {
             Long dateLong = cursor.getLong(cursor.getColumnIndex(mHelper.COL_DATE));
             DateTime datetime = new DateTime(new Date(dateLong));
 
-            Event event = new Event(title, detail, datetime);
+            Event event = new Event(id, title, detail, datetime);
 
             mEventList.add(event);
         }
+        sortList();
         cursor.close();
+    }
+
+    private static void sortList(){
+        ArrayList<Event> sortArray = new ArrayList<>();
+        CardsColour c = CardsColour.getInstance();
+        for(Event e: mEventList){
+            if(c.getColour(e.getDate()) == R.color.codeRed) sortArray.add(e);
+        }
+        for(Event e: mEventList){
+            if(c.getColour(e.getDate()) == R.color.codeOrange) sortArray.add(e);
+        }
+        for(Event e: mEventList){
+            if(c.getColour(e.getDate()) == R.color.codeYellow) sortArray.add(e);
+        }
+        for(Event e: mEventList){
+            if(c.getColour(e.getDate()) == R.color.codeGreen) sortArray.add(e);
+        }
+        for(Event e: mEventList){
+            if(c.getColour(e.getDate()) == R.color.codeGrey) sortArray.add(e);
+        }
+        mEventList = sortArray;
     }
 
     public static ArrayList<Event> getEventList() {
