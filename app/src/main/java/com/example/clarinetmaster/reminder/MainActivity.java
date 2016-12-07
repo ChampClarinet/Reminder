744dd502d7716e93f537a4ac645dcac55901d2d7
Wallop Opasakhun <champbassist159@gmail.com>
2016-12-07 23:39:02 +0700

    bug fixes on drawable

--- app/src/main/java/com/example/clarinetmaster/reminder/MainActivity.java
+++ app/src/main/java/com/example/clarinetmaster/reminder/MainActivity.java
@@ -1,28 +1,18 @@
 package com.example.clarinetmaster.reminder;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.KeyEvent;
-import android.view.LayoutInflater;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.inputmethod.EditorInfo;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.clarinetmaster.reminder.Tools.CardsColour;
-import com.example.clarinetmaster.reminder.Tools.DateTime;
+import com.example.clarinetmaster.reminder.Adapters.CardAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
         feedRecyclerView = (RecyclerView) findViewById(R.id.feedRecycler);
+        CardAdapter adapter = new CardAdapter(this);
+        feedRecyclerView.setAdapter(adapter);
 
     }
 
